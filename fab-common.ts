58baa49fb2ad1b2a6b068373af0a867da80dd757
Brad Martin <bradwaynemartin@gmail.com>
2017-06-14 12:53:11 -0500

    add prettier

--- fab-common.ts
+++ fab-common.ts
@@ -1,8 +1,5 @@
 /**************************************************************************************
  * Made for the {N} community by Brad Martin @BradWayneMartin
- * Thanks to Lazaro Danillo for his contributions - https://github.com/lazaromenezes
- * Thanks to Steve McNiven-Scott for his contributions - https://github.com/sitefinitysteve
- * Thanks to Gabriel Marinho for his contributions - https://github.com/gabrielbiga
  * https://twitter.com/BradWayneMartin
  * https://github.com/bradmartin
  * Pull requests are welcome. Enjoy!
@@ -13,173 +10,184 @@ import { Color } from "color";
 import { PanGestureEventData } from "ui/gestures";
 
 export class FloatingActionButtonBase extends View implements definitions.Fab {
+  private swipeEventAttached: boolean = false;
+  public hideOnSwipeOfView: string;
+  public swipeAnimation:
+    | "slideUp"
+    | "slideDown"
+    | "slideRight"
+    | "slideLeft"
+    | "scale";
+  public hideAnimationDuration: number;
 
-    private swipeEventAttached: boolean = false;
-    public hideOnSwipeOfView: string;
-    public swipeAnimation: "slideUp" | "slideDown" | "slideRight" | "slideLeft" | "scale";
-    public hideAnimationDuration: number;
+  public rippleColor: Color;
+  public icon: string;
+  public backColor: Color;
 
-    public rippleColor: Color;
-    public icon: string;
-    public backColor: Color;
-
-    getDurationDefault(animationType: string) {
-        switch (animationType) {
-            case "scale":
-                return 100;
-            default:
-                return 300;
-        }
+  getDurationDefault(animationType: string) {
+    switch (animationType) {
+      case "scale":
+        return 100;
+      default:
+        return 300;
     }
+  }
 
-    onLoaded() {
-        super.onLoaded()
+  onLoaded() {
+    super.onLoaded();
 
-        if (this.swipeEventAttached === false) {
-            var fab = this;
-            if (this.hideOnSwipeOfView !== undefined) {
-                var swipeItem = this.page.getViewById(this.hideOnSwipeOfView);
-                var animationType = (this.swipeAnimation) ? this.swipeAnimation : "slideDown"
+    if (this.swipeEventAttached === false) {
+      var fab = this;
+      if (this.hideOnSwipeOfView !== undefined) {
+        var swipeItem = this.page.getViewById(this.hideOnSwipeOfView);
+        var animationType = this.swipeAnimation
+          ? this.swipeAnimation
+          : "slideDown";
 
-                if (swipeItem !== undefined) {
-                    var duration = (this.hideAnimationDuration) ? this.hideAnimationDuration : this.getDurationDefault(animationType);
+        if (swipeItem !== undefined) {
+          var duration = this.hideAnimationDuration
+            ? this.hideAnimationDuration
+            : this.getDurationDefault(animationType);
 
-                    swipeItem.on("pan", (args: PanGestureEventData) => {
-                        //Swipe up
-                        if (args.deltaY < -10) {
-                            switch (animationType) {
-                                case "slideUp":
-                                    fab.animate({
-                                        translate: {
-                                            x: 0,
-                                            y: -200
-                                        },
-                                        opacity: 0,
-                                        duration: duration
-                                    });
-                                    break;
-                                case "slideDown":
-                                    fab.animate({
-                                        translate: {
-                                            x: 0,
-                                            y: 200
-                                        },
-                                        opacity: 0,
-                                        duration: duration
-                                    });
-                                    break;
-                                case "slideRight":
-                                    fab.animate({
-                                        translate: {
-                                            x: 200,
-                                            y: 0
-                                        },
-                                        opacity: 0,
-                                        duration: duration
-                                    });
-                                    break;
-                                case "slideLeft":
-                                    fab.animate({
-                                        translate: {
-                                            x: -200,
-                                            y: 0
-                                        },
-                                        opacity: 0,
-                                        duration: duration
-                                    });
-                                    break;
-                                case "scale":
-                                    fab.animate({
-                                        scale: {
-                                            x: 0,
-                                            y: 0
-                                        },
-                                        duration: duration
-                                    });
-                                    break;
-                            }
-
-                        }
-                        //Swipe Down
-                        else if (args.deltaY > 0) {
-                            switch (animationType) {
-                                case "slideUp":
-                                    fab.animate({
-                                        translate: {
-                                            x: 0,
-                                            y: 0
-                                        },
-                                        opacity: 1,
-                                        duration: duration
-                                    });
-                                    break;
-                                case "slideDown":
-                                    fab.animate({
-                                        translate: {
-                                            x: 0,
-                                            y: 0
-                                        },
-                                        opacity: 1,
-                                        duration: duration
-                                    });
-                                    break;
-                                case "slideRight":
-                                    fab.animate({
-                                        translate: {
-                                            x: 0,
-                                            y: 0
-                                        },
-                                        opacity: 1,
-                                        duration: duration
-                                    });
-                                    break;
-                                case "slideLeft":
-                                    fab.animate({
-                                        translate: {
-                                            x: 0,
-                                            y: 0
-                                        },
-                                        opacity: 1,
-                                        duration: duration
-                                    });
-                                    break;
-                                case "scale":
-                                    fab.animate({
-                                        scale: {
-                                            x: 1,
-                                            y: 1
-                                        },
-                                        duration: duration
-                                    });
-                                    break;
-                            }
-                        };
-                    });
-
-                    this.swipeEventAttached = true;
-                }
+          swipeItem.on("pan", (args: PanGestureEventData) => {
+            //Swipe up
+            if (args.deltaY < -10) {
+              switch (animationType) {
+                case "slideUp":
+                  fab.animate({
+                    translate: {
+                      x: 0,
+                      y: -200
+                    },
+                    opacity: 0,
+                    duration: duration
+                  });
+                  break;
+                case "slideDown":
+                  fab.animate({
+                    translate: {
+                      x: 0,
+                      y: 200
+                    },
+                    opacity: 0,
+                    duration: duration
+                  });
+                  break;
+                case "slideRight":
+                  fab.animate({
+                    translate: {
+                      x: 200,
+                      y: 0
+                    },
+                    opacity: 0,
+                    duration: duration
+                  });
+                  break;
+                case "slideLeft":
+                  fab.animate({
+                    translate: {
+                      x: -200,
+                      y: 0
+                    },
+                    opacity: 0,
+                    duration: duration
+                  });
+                  break;
+                case "scale":
+                  fab.animate({
+                    scale: {
+                      x: 0,
+                      y: 0
+                    },
+                    duration: duration
+                  });
+                  break;
+              }
+            } else if (args.deltaY > 0) {
+              //Swipe Down
+              switch (animationType) {
+                case "slideUp":
+                  fab.animate({
+                    translate: {
+                      x: 0,
+                      y: 0
+                    },
+                    opacity: 1,
+                    duration: duration
+                  });
+                  break;
+                case "slideDown":
+                  fab.animate({
+                    translate: {
+                      x: 0,
+                      y: 0
+                    },
+                    opacity: 1,
+                    duration: duration
+                  });
+                  break;
+                case "slideRight":
+                  fab.animate({
+                    translate: {
+                      x: 0,
+                      y: 0
+                    },
+                    opacity: 1,
+                    duration: duration
+                  });
+                  break;
+                case "slideLeft":
+                  fab.animate({
+                    translate: {
+                      x: 0,
+                      y: 0
+                    },
+                    opacity: 1,
+                    duration: duration
+                  });
+                  break;
+                case "scale":
+                  fab.animate({
+                    scale: {
+                      x: 1,
+                      y: 1
+                    },
+                    duration: duration
+                  });
+                  break;
+              }
             }
+          });
+
+          this.swipeEventAttached = true;
         }
-    };
+      }
+    }
+  }
 }
 
-
 export const backColorProperty = new Property<FloatingActionButtonBase, Color>({
-    name: "backColor",
-    equalityComparer: Color.equals,
-    valueConverter: (v) => new Color(v),
-    valueChanged: (fab, oldValue, newValue) => {
-        fab.style.backgroundColor = newValue
-    }
+  name: "backColor",
+  equalityComparer: Color.equals,
+  valueConverter: v => new Color(v),
+  valueChanged: (fab, oldValue, newValue) => {
+    fab.style.backgroundColor = newValue;
+  }
 });
 backColorProperty.register(FloatingActionButtonBase);
 
 export const iconProperty = new Property<FloatingActionButtonBase, string>({
-    name: "icon", affectsLayout: true
+  name: "icon",
+  affectsLayout: true
 });
 iconProperty.register(FloatingActionButtonBase);
 
-export const rippleColorProperty = new Property<FloatingActionButtonBase, Color>({
-    name: "rippleColor", equalityComparer: Color.equals, valueConverter: (v) => new Color(v)
+export const rippleColorProperty = new Property<
+  FloatingActionButtonBase,
+  Color
+>({
+  name: "rippleColor",
+  equalityComparer: Color.equals,
+  valueConverter: v => new Color(v)
 });
 rippleColorProperty.register(FloatingActionButtonBase);
--- fab.android.ts
+++ fab.android.ts
@@ -1,79 +1,103 @@
-﻿import { FloatingActionButtonBase, backColorProperty, rippleColorProperty, iconProperty } from "./fab-common";
+import {
+  FloatingActionButtonBase,
+  backColorProperty,
+  rippleColorProperty,
+  iconProperty
+} from "./fab-common";
 import * as style from "ui/styling/style";
 import * as utils from "utils/utils";
 import { Color } from "color";
 import * as ImageSource from "image-source";
-import { backgroundColorProperty, backgroundInternalProperty } from "ui/core/view";
+import {
+  backgroundColorProperty,
+  backgroundInternalProperty
+} from "ui/core/view";
 
 declare var android: any;
 
 export class Fab extends FloatingActionButtonBase {
-    public static tapEvent = "tap";
+  public static tapEvent = "tap";
 
-    public createNativeView() {
-        const btn = new android.support.design.widget.FloatingActionButton(this._context);
-        let that = new WeakRef(this);
-        btn.setOnClickListener(new android.view.View.OnClickListener({
-            get owner() {
-                return that.get();
-            },
+  public createNativeView() {
+    const btn = new android.support.design.widget.FloatingActionButton(
+      this._context
+    );
+    let that = new WeakRef(this);
+    btn.setOnClickListener(
+      new android.view.View.OnClickListener({
+        get owner() {
+          return that.get();
+        },
 
-            onClick: function (v) {
-                if (this.owner) {
-                    this.owner._emit("tap");
-                }
-            }
-        }));
-
-        return btn;
-    };
-
-    [backColorProperty.setNative](value: Color) {
-        this.nativeView.setBackgroundTintList(android.content.res.ColorStateList.valueOf(value.android));
-    }
-
-    [backgroundColorProperty.getDefault](): android.content.res.ColorStateList {
-        return this.nativeView.getBackgroundTintList();
-    }
-
-    [backgroundColorProperty.setNative](value: Color | android.content.res.ColorStateList) {
-        let newValue: android.content.res.ColorStateList;
-        if (value instanceof Color) {
-            newValue = android.content.res.ColorStateList.valueOf(value.android);
-        } else {
-            // Resetting with the default value;
-            newValue = value;
+        onClick: function(v) {
+          if (this.owner) {
+            this.owner._emit("tap");
+          }
         }
-        this.nativeView.setBackgroundTintList(newValue);
+      })
+    );
+
+    return btn;
+  }
+
+  [backColorProperty.setNative](value: Color) {
+    this.nativeView.setBackgroundTintList(
+      android.content.res.ColorStateList.valueOf(value.android)
+    );
+  }
+
+  [backgroundColorProperty.getDefault](): android.content.res.ColorStateList {
+    return this.nativeView.getBackgroundTintList();
+  }
+
+  [backgroundColorProperty.setNative](
+    value: Color | android.content.res.ColorStateList
+  ) {
+    let newValue: android.content.res.ColorStateList;
+    if (value instanceof Color) {
+      newValue = android.content.res.ColorStateList.valueOf(value.android);
+    } else {
+      // Resetting with the default value;
+      newValue = value;
     }
+    this.nativeView.setBackgroundTintList(newValue);
+  }
 
-    [backgroundInternalProperty.setNative](value: any) {
-        //NOOP
+  [backgroundInternalProperty.setNative](value: any) {
+    //NOOP
+  }
+
+  [rippleColorProperty.setNative](value: Color) {
+    this.nativeView.setRippleColor(value.android);
+  }
+
+  [iconProperty.setNative](value: any) {
+    let iconDrawable = null;
+
+    if (ImageSource.isFileOrResourcePath(value)) {
+      iconDrawable = ImageSource.fromFileOrResource(value);
+      if (iconDrawable) {
+        this.nativeView.setImageBitmap(iconDrawable.android);
+      } else {
+        console.log(
+          "The icon: " + value + " was not found. Check your XML icon property."
+        );
+      }
+    } else {
+      const drawableId = android.content.res.Resources
+        .getSystem()
+        .getIdentifier(value, "drawable", "android");
+      iconDrawable = android.content.res.Resources
+        .getSystem()
+        .getDrawable(drawableId);
+
+      if (iconDrawable) {
+        this.nativeView.setImageDrawable(iconDrawable);
+      } else {
+        console.log(
+          "The icon: " + value + " was not found. Check your XML icon property."
+        );
+      }
     }
-
-    [rippleColorProperty.setNative](value: Color) {
-        this.nativeView.setRippleColor(value.android);
-    }
-
-    [iconProperty.setNative](value: any) {
-        let iconDrawable = null;
-
-        if (ImageSource.isFileOrResourcePath(value)) {
-            iconDrawable = ImageSource.fromFileOrResource(value);
-            if (iconDrawable) {
-                this.nativeView.setImageBitmap(iconDrawable.android);
-            } else {
-                console.log("The icon: " + value + " was not found. Check your XML icon property.");
-            }
-        } else {
-            const drawableId = android.content.res.Resources.getSystem().getIdentifier(value, "drawable", "android");
-            iconDrawable = android.content.res.Resources.getSystem().getDrawable(drawableId);
-
-            if (iconDrawable) {
-                this.nativeView.setImageDrawable(iconDrawable);
-            } else {
-                console.log("The icon: " + value + " was not found. Check your XML icon property.");
-            }
-        }
-    }
+  }
 }
