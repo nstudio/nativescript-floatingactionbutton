080d954c97c4232b6b50ae5ba2d750148e8020fa
Brad Martin <bradwaynemartin@gmail.com>
2018-11-09 22:37:30 -0600

    use typings

--- src/fab.ios.ts
+++ src/fab.ios.ts
@@ -1,14 +1,12 @@
 import * as ImageSource from 'tns-core-modules/image-source';
 import { FloatingActionButtonBase, iconProperty } from './fab-common';
 
-declare var MNFloatingActionButton: any;
-
 export class Fab extends FloatingActionButtonBase {
 	public nativeView: UIView;
 
 	constructor() {
 		super();
-		const btn = MNFloatingActionButton.alloc().init();
+		const btn = MNFloatingActionButton.alloc().init() as MNFloatingActionButton;
 		btn.animationScale = 0.95;
 		this.nativeView = btn;
 	}
@@ -21,24 +19,28 @@ export class Fab extends FloatingActionButtonBase {
 
 			// Set the new one
 			if (iconDrawable !== null) {
-				newImageView = UIImageView.alloc().initWithImage(iconDrawable.ios);
+				newImageView = UIImageView.alloc().initWithImage(
+					iconDrawable.ios
+				) as UIImageView;
 			}
 		} else {
 			// Default image
 			const defaultImage = ImageSource.fromBase64(
 				'iVBORw0KGgoAAAANSUhEUgAAAJAAAACQAQAAAADPPd8VAAAAAnRSTlMAAHaTzTgAAAAqSURBVHgBY6AMjIJRYP9n0AuNCo0KMf+HgwPDTmgoRMeo0KgQRWAUjAIABsnZRR7bYyUAAAAASUVORK5CYII='
-			);
+			) as ImageSource.ImageSource;
 
-			newImageView = UIImageView.alloc().initWithImage(defaultImage.ios);
+			newImageView = UIImageView.alloc().initWithImage(
+				defaultImage.ios
+			) as UIImageView;
 		}
 
 		if (newImageView !== null) {
 			// Kill the old Image, cocoapod doesn't support changing it yet
-			const button = this.nativeView.subviews[0];
-			const oldBadImageView = button.subviews[0];
+			const button = this.nativeView.subviews[0] as MNFloatingActionButton;
+			const oldBadImageView = button.subviews[0]; // this should be the image view inside the MNFloatingActionButton
 			oldBadImageView.removeFromSuperview();
 
-			// Add the new guy
+			// Add the new image to the button
 			button.addSubview(newImageView);
 		}
 	}
@@ -50,16 +52,16 @@ export class Fab extends FloatingActionButtonBase {
 		bottom: number
 	): void {
 		super.onLayout(left, top, right, bottom);
-		this.centerIcon();
+		this._centerIcon();
 	}
 
-	centerIcon() {
-		const frame = this.nativeView.frame;
-		const width = frame.size.width;
-		const height = frame.size.height;
+	private _centerIcon() {
+		const frame = this.nativeView.frame as CGRect;
+		const width = frame.size.width as number;
+		const height = frame.size.height as number;
 
-		const button = this.nativeView.subviews[0];
-		const imageView = <UIImageView>button.subviews[0];
+		const button = this.nativeView.subviews[0] as MNFloatingActionButton;
+		const imageView = <UIImageView>button.subviews[0]; // should be the image view inside the MNFloatingActionButton
 
 		imageView.contentMode = UIViewContentMode.ScaleAspectFit;
 		imageView.frame = CGRectMake(0, 0, width / 2, height / 2);
---
+++ src/typings/objc!MNFloatingActionButton.d.ts
@@ -0,0 +1,35 @@
+
+declare class MNFloatingActionButton extends UIControl {
+
+	static alloc(): MNFloatingActionButton; // inherited from NSObject
+
+	static appearance(): MNFloatingActionButton; // inherited from UIAppearance
+
+	static appearanceForTraitCollection(trait: UITraitCollection): MNFloatingActionButton; // inherited from UIAppearance
+
+	static appearanceForTraitCollectionWhenContainedIn(trait: UITraitCollection, ContainerClass: typeof NSObject): MNFloatingActionButton; // inherited from UIAppearance
+
+	static appearanceForTraitCollectionWhenContainedInInstancesOfClasses(trait: UITraitCollection, containerTypes: NSArray<typeof NSObject> | typeof NSObject[]): MNFloatingActionButton; // inherited from UIAppearance
+
+	static appearanceWhenContainedIn(ContainerClass: typeof NSObject): MNFloatingActionButton; // inherited from UIAppearance
+
+	static appearanceWhenContainedInInstancesOfClasses(containerTypes: NSArray<typeof NSObject> | typeof NSObject[]): MNFloatingActionButton; // inherited from UIAppearance
+
+	static new(): MNFloatingActionButton; // inherited from NSObject
+
+	animationDuration: number;
+
+	animationScale: number;
+
+	centerImage: UIImage;
+
+	shadowColor: UIColor;
+
+	shadowOpacity: number;
+
+	shadowRadius: number;
+}
+
+declare var MNFloatingActionButtonVersionNumber: number;
+
+declare var MNFloatingActionButtonVersionString: interop.Reference<number>;
